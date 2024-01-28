4454f078427e845bf6ad2455a0d2896292396dfd
nimus100 <nimus100@proton.me>
2024-01-28 14:54:34 +0100

    fixed emit.png

--- JokeToKill/Util/SceneryObject.cs
+++ JokeToKill/Util/SceneryObject.cs
@@ -17,6 +17,9 @@ namespace JokeToKill.Util
             var size = Constants.CamSize * Constants.AspectVec;
             this.CreateDrawableChild(Sprites.SceneBG, localScale: size, drawOrder: -10f);
             this.CreateDrawableChild(Sprites.BattleBG, localScale: size, drawOrder: -2f);
+            this.CreateDrawableChild(Sprites.PlayerStatic, 
+                localPosition: new Vector2(-3.5f, 0.3f),
+                localScale: Sprites.GetSpriteSize(Sprites.PlayerStatic) * 2, drawOrder: -1.5f);
             this.CreateDrawableChild(Sprites.BagBG, localScale: size, drawOrder: -1f);
             this.CreateDrawableChild(Sprites.BagFG, localScale: size, drawOrder: 10f);
             
